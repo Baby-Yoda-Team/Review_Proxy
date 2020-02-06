e9cbb756602584e36c29b52b1e466f8cd4b8d4ae
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-02-05 17:20:21 -0800

    added modules

--- server/server.js
+++ server/server.js
@@ -2,11 +2,8 @@ const express = require('express');
 let app = express();
 const port = 4000;
 const path = require('path');
-var proxy = require('http-proxy-middleware');
 
-app.use(express.static(path.join(__dirname, './')));
-
-app.use('/', proxy({ target: 'http://127.0.0.1:8081/', changeOrigin: true }));
+app.use(express.static(path.join(__dirname, '.')));
 
 app.listen(port, () => {
   console.log(`proxy server listening on port ${port}`);
